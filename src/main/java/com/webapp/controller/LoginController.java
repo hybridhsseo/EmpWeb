4e4d0a34dcde509c9a5d19d53455011be0afd4fa
teacher <protocls@nate.com>
2015-04-27 17:29:31 +0900

    xxx

--- src/main/java/com/webapp/controller/LoginController.java
+++ src/main/java/com/webapp/controller/LoginController.java
@@ -1,16 +1,57 @@
 package com.webapp.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.webapp.model.User;
 
+@Controller
+@RequestMapping("/login")
 public class LoginController {
 
+	static Log log = LogFactory.getLog(LoginController.class);
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login() {
+		log.info("#########################");
+		log.info("login() GET...");
+		log.info("#########################");
 		
 		return "login/loginform";
 	}
 	
-	public String login(User user) {
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(User user, HttpSession session) {
+		log.info("#########################");
+		log.info("login() POST...");
+		log.info("id=" + user.getId());
+		log.info("password=" + user.getPassword());
+		log.info("#########################");
 		
-		return "login/success";
+		if ("webapp".equals(user.getId()) && 
+			"1234".equals(user.getPassword())) {
+			// Login success
+			session.setAttribute("user", user);
+			return "login/success";
+		} else {
+			// Login Fail
+			return "redirect:login";
+		}
+		
 	}
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public String logout(HttpSession session) {
+		
+		session.invalidate();
+		
+		return "login/logoutsuccess";
+	}
+	
+	
 }
