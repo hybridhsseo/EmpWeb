0b76ab1fb70fcdb870086f4ddf8ebe80dd62f33f
teacher <protocls@nate.com>
2015-05-06 12:29:42 +0900

    xx

--- src/main/webapp/login3/js/about.js
+++ src/main/webapp/login3/js/about.js
@@ -6,4 +6,5 @@ app.controller("aboutController", function($scope, $http) {
 //		alert("aboutController");
 	console.log("aboutController start...");
 	$scope.$parent.pageClass = 'page-about';
+
 });
