33e3bdb2f2718826055b2af44cdd3087d801076f
teacher <protocls@nate.com>
2015-05-11 14:00:40 +0900

    xxx

---
+++ src/main/java/com/webapp/model/tour/ResponseMessage2.java
@@ -0,0 +1,16 @@
+package com.webapp.model.tour;
+
+import java.util.Map;
+
+public class ResponseMessage2 {
+	Response2 response;
+
+	public Response2 getResponse() {
+		return response;
+	}
+
+	public void setResponse(Response2 response) {
+		this.response = response;
+	}
+	
+}
--- src/test/java/com/webapp/GsonTest.java
+++ src/test/java/com/webapp/GsonTest.java
@@ -12,60 +12,38 @@ import org.junit.Test;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import com.webapp.model.User;
 import com.webapp.model.tour.Items;
 import com.webapp.model.tour.ResponseMessage;
+import com.webapp.model.tour.ResponseMessage2;
 
 public class GsonTest {
 
 	@Test
-	public void test1() {
+	public void test() throws IOException {
 		Gson g = new GsonBuilder().create();
-		
-		System.out.println(g.toJson("hello"));
-		System.out.println(g.toJson(1900.));
-		System.out.println(g.toJson(true));
-		
-		User u = new User();
-		u.setId("100");
-		u.setPassword("1234");
-		System.out.println(g.toJson(u));
-		
-		Map<Object, Object> map = new HashMap<Object, Object>();
-		map.put("code", 123);
-		map.put("name", "한국관광공사");
-		
-		System.out.println(g.toJson(map));
-		
-	}
-	
-	@Test
-	public void test2() {
-		Gson g = new GsonBuilder().create();
-		String json = "{\"id\":\"100\",\"password\":\"1234\"}";
-		
-		Map<Object, Object> map = g.fromJson(json, Map.class);
-		
-		System.out.println(map.get("id"));
-		System.out.println(map.get("password"));
-		
-	}
-	@Test
-	public void test3() throws IOException {
-		Gson g = new GsonBuilder().create();
-		String url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?keyword=hotel&numOfRows=20&pageNo=1&_type=json&MobileOS=ETC&MobileApp=Emp&ServiceKey=sA7tgy37XyQzBU2fPZpZw%2BGKNlR0BPdgP2RhAvNrw4ls2so%2F%2BgeLDAT8AHJO6CacIlHvKIfubhwPjiDXpy%2B7%2Fw%3D%3D&keyword=사찰";
+//		String url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?keyword=hotel&numOfRows=20&pageNo=1&_type=json&MobileOS=ETC&MobileApp=Emp&ServiceKey=sA7tgy37XyQzBU2fPZpZw%2BGKNlR0BPdgP2RhAvNrw4ls2so%2F%2BgeLDAT8AHJO6CacIlHvKIfubhwPjiDXpy%2B7%2Fw%3D%3D&keyword=사찰";
+		String url = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?numOfRows=20&pageNo=1&_type=json&MobileOS=ETC&MobileApp=Emp&ServiceKey=sA7tgy37XyQzBU2fPZpZw%2BGKNlR0BPdgP2RhAvNrw4ls2so%2F%2BgeLDAT8AHJO6CacIlHvKIfubhwPjiDXpy%2B7%2Fw%3D%3D&keyword=사찰";
 		
 		URL u = new URL(url);
 		InputStream in = u.openStream();
 		Scanner scan = new Scanner(in);
-		while(scan.hasNextLine())
-			System.out.println(scan.nextLine());
+//		while(scan.hasNextLine())
+//			System.out.println(scan.nextLine());
+//		
+		ResponseMessage rm=null;
+		ResponseMessage2 rm2=null;
+		try {
+			rm = g.fromJson(new InputStreamReader(u.openStream()), ResponseMessage.class);
+			g.toJson(rm, System.out);
+		} catch (JsonSyntaxException e) {
+			rm2 = g.fromJson(new InputStreamReader(u.openStream()), ResponseMessage2.class);
+			g.toJson(rm2, System.out);
+//			e.printStackTrace();
+		}
 		
 		
-		ResponseMessage rm = g.fromJson(new InputStreamReader(u.openStream()), ResponseMessage.class);
-		
-//		System.out.println(g.toJson(item));
-		g.toJson(rm, System.out);
 	}
-	
 }
+
